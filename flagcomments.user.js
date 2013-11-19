14a2723427275d7bdd61ab62c691c3d5750b3ebe
Undo1 <parker@erwaysoftware.com>
2013-11-19 14:35:48 -0700

    added goodies

---
+++ flagcomments.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name Comment purge
+// @version 1.0.1
+// @author Undo (with help from @ManishEarth)
+// @description Quick & easy comment flagging
+// @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
+// @include http://*stackoverflow.com/*
+// @include http://*superuser.com/*
+// @include http://*serverfault.com/*
+// @include http://*stackexchange.com/*
+// @include http://discuss.area51.com/*
+// ==/UserScript==
+
+function with_jquery(f) {
+     var script = document.createElement("script");
+     script.type = "text/javascript";
+     script.textContent = "(" + f.toString() + ")(jQuery)";
+     document.body.appendChild(script);
+};
+
+
+with_jquery(function($){
+$('document').ready(function(){
+    var htm=$('.post-menu').html()||"";
+    if(htm!=-1&&$('[id^=close-question] span').length!=0){
+        return;
+    }
+    var isShowingCheckboxes = false;
+$('.post-menu').append($('<span class="lsep">|</span><a class="purge" href="javascript:void(0)" title="Flag obsolete comments">autoflag</a>'));
+$('.purge').bind("click",function(){
+
+	// var postid=$(this).closest('div.question,div[id^=answer]').data('questionid')||$(this).closest('div.question,div[id^=answer]').data('answerid');
+	// $.post('/flags/posts/'+postid+'/add/PostOther',{'otherText':'obsolete comments',
+	// 'fromToolsQueue':'false',
+	// 'target-post-id':postid,
+	// 'fkey':StackExchange.options.user.fkey});
+	if (isShowingCheckboxes == false)
+	{
+		$(this).html("<strong>confirm</strong>");
+		$('.comment').prepend("<input type='checkbox' class='autoflag_checkbox'>");
+	}
+	else
+	{
+		var checked = $('input:checked');
+		console.log(checked.length);
+		$(".autoflag_checkbox").remove();
+		$(this).html("<strong>0/" + checked.length + "...</strong>");
+		$.delay(1000);
+		$(this).html("<strong>1/" + checked.length + "...</strong>");
+	}
+	isShowingCheckboxes = !isShowingCheckboxes;
+	return false;
+});
+});
+});
