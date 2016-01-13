82208f8942505baa26dfd3e793e2e03412e0309d
Undo1 <parker@erwaysoftware.com>
2016-01-12 21:53:21 -0700

    Fix indentation and general formatting

--- deletefromcommenthistory.user.js
+++ deletefromcommenthistory.user.js
@@ -12,55 +12,61 @@
 // ==/UserScript==
 
 function with_jquery(f) {
-     var script = document.createElement("script");
-     script.type = "text/javascript";
-     script.textContent = "(" + f.toString() + ")(jQuery)";
-     document.body.appendChild(script);
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.textContent = "(" + f.toString() + ")(jQuery)";
+  document.body.appendChild(script);
 };
 
 
 with_jquery(function($){
-	$('document').ready(function(){
-		$('.meta-row .creation-date').each( function(){
-			if ($(this).closest('tr.meta-row').hasClass("deleted-row") == false){
-			$(this).append($('<span class="lsep">|</span><a class="delete-comment" href="javascript:void(0)" title="Delete comment">(delete)</a>'));
-		}else{
-			$(this).append($('<span class="lsep">|</span><a class="delete-comment" href="javascript:void(0)" title="Delete comment">(undelete)</a>'));
-		}
-		});
-		$('.delete-comment').bind("click",function(){
-      			var deleteButton = $(this);
-			var commentid=$(this).closest('tr.meta-row').data('id');
-      			console.log(commentid);
-			var postid = $(this).closest('tr.meta-row').data('postid');
-      			console.log(postid);
+  $('document').ready(function(){
+    $('.meta-row .creation-date').each(function() {
+      if ($(this).closest('tr.meta-row').hasClass("deleted-row") == false)
+      {
+        $(this).append($('<span class="lsep">|</span><a class="delete-comment" href="javascript:void(0)" title="Delete comment">(delete)</a>'));
+      }
+      else
+      {
+        $(this).append($('<span class="lsep">|</span><a class="delete-comment" href="javascript:void(0)" title="Delete comment">(undelete)</a>'));
+      }
+    });
 
-			if ($(this).closest('tr.meta-row').hasClass("deleted-row") == true){
-				var url = "/admin/posts/" + postid + "/comments/" + commentid + "/undelete"
-				$(this).html("<strong>(working...)</strong>");
-				$.post(url,
-					{'fkey':StackExchange.options.user.fkey, 'sendCommentBackInMessage':'true'},
-					function(data){
-							deleteButton.html("(delete)");
-							deleteButton.closest('tr.meta-row').removeClass('deleted-row')
-						}
-				);
-			}else{
-				$(this).html("<strong>(working...)</strong>");
-				var url = '/posts/comments/'+commentid+'/vote/10'
-				$.post(url,
-					{'fkey':StackExchange.options.user.fkey, 'sendCommentBackInMessage':'true'},
-					function(data){
-						console.log(data);
-						if (data.Success == true)
-						{
-							deleteButton.html("(undelete)");
-							deleteButton.closest('tr.meta-row').addClass('deleted-row')
-						}
-					}
-				);
-			}
-		});
-		return false;
-	});
+    $('.delete-comment').bind("click", function() {
+      var deleteButton = $(this);
+      var commentid=$(this).closest('tr.meta-row').data('id');
+      console.log(commentid);
+      var postid = $(this).closest('tr.meta-row').data('postid');
+      console.log(postid);
+
+      if ($(this).closest('tr.meta-row').hasClass("deleted-row") == true){
+        var url = "/admin/posts/" + postid + "/comments/" + commentid + "/undelete"
+        $(this).html("<strong>(working...)</strong>");
+        $.post(url,
+          {'fkey': StackExchange.options.user.fkey, 'sendCommentBackInMessage': 'true'},
+          function(data){
+            deleteButton.html("(delete)");
+            deleteButton.closest('tr.meta-row').removeClass('deleted-row')
+          }
+        );
+      }
+      else
+      {
+        $(this).html("<strong>(working...)</strong>");
+        var url = '/posts/comments/'+commentid+'/vote/10'
+        $.post(url,
+          {'fkey': StackExchange.options.user.fkey, 'sendCommentBackInMessage': 'true'},
+          function(data){
+            console.log(data);
+            if (data.Success == true)
+            {
+              deleteButton.html("(undelete)");
+              deleteButton.closest('tr.meta-row').addClass('deleted-row')
+            }
+          }
+          );
+      }
+    });
+    return false;
+  });
 });
