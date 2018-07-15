5f978f923683ddf26f38333239b431f65d095a09
Undo1 <Undo1@users.noreply.github.com>
2018-07-14 19:10:07 -0600

    no way this works

---
+++ ml-monitor.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name FriendlyMods
+// @version 01
+// @author Undo
+// @description Show ML results inline
+// @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
+// @include http://*stackoverflow.com/*
+// ==/UserScript==
+
+function with_jquery(f) {
+     var script = document.createElement("script");
+     script.type = "text/javascript";
+     script.textContent = "(" + f.toString() + ")(jQuery)";
+     document.body.appendChild(script);
+};
+
+
+with_jquery(function($){
+	$('document').ready(function(){
+
+		var body = $(".post-text")[0].innerHTML
+    var title = $("#question-header .question-hyperlink")[0].innerHTML
+    $.get("https://tmpfiles.dvtk.me/", { "title": title, "body": body }, function(data) {
+      var probOfRecRequest = data["id"][data["class"].indexOf("Recommendation request")];
+      
+      if (probOfRecRequest > 0.5) {
+        $(".inner-content.clearfix").prepend('<span style="background-color: red; padding: 3px; color: white;">recommendation request (' + probOfRecRequest * 100 + '%)</span>')
+      } else {
+        $(".inner-content.clearfix").prepend('<span style="background-color: green; padding: 3px; color: white;">not-recommendation request (' + probOfRecRequest * 100 + '%)</span>')
+      }
+    });
+		
+		return false;
+	});
+});
